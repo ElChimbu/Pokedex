95b39dda7630a25c887bbeeaa76e65719c30c98d
Jeremias Gauna <gaunajeremiasgauna@gmail.com>
2019-08-20 13:07:54 -0300

    Refactor
    Trabajando en una lista de pokemons

---
+++ app/localStorage.js
@@ -0,0 +1,17 @@
+Array.prototype.unique=function(a){
+    return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+  });
+
+export const pokeLocalStorage = (pokemonArray) =>{
+    let newPokemonArray = pokemonArray.unique()
+
+    localStorage.setItem('Pokemon', newPokemonArray);
+
+    getPokemon()
+}
+
+ const getPokemon = () =>{
+    const get = localStorage.getItem('Pokemon');
+    let insideArray = [get]
+    console.log(insideArray)
+}
